ea20edfd6b7ea91cc3186380ae12c47b61b0b55a
Sam Thomson <samt@samt.st>
2018-04-04 07:39:33 +0100

    disable commenting

--- readme.md
+++ readme.md
@@ -8,10 +8,8 @@ copy `.env.sample` to `.env` and add desired values.
 
 `docker-compose up`
 
-
 website: http://localhost:80
 phpmyadin: http://localhost:8080
-
 
 ## todo
 
@@ -19,3 +17,4 @@ phpmyadin: http://localhost:8080
 - nice theme
 - disable unneeded stuff in admin section
 - add icon to gallery post type
+- css / js includes
--- www-workspace/wp-content/themes/picture-site/functions.php
+++ www-workspace/wp-content/themes/picture-site/functions.php
@@ -2,7 +2,20 @@
 
     // disable top admin bar from showing
     show_admin_bar(false);
-
+    // disable comments and ping
+    update_option('default_comment_status', '');
+    update_option('default_ping_status', '');
+    // remove comments menu item from admin section
+    add_action( 'admin_init', 'my_remove_admin_menus' );
+    function my_remove_admin_menus() {
+        remove_menu_page( 'edit-comments.php' );
+    }
+    // Remove comments from post and pages
+    add_action('init', 'remove_comment_support', 100);
+    function remove_comment_support() {
+        remove_post_type_support('post', 'comments');
+        remove_post_type_support('page', 'comments');
+    }
 
     // set the default permalink structure for pages
     add_action( 'init', function() {
@@ -12,16 +25,16 @@
 
     // create the 'gallery' post type
     function create_post_type() {
-        register_post_type( 'ps_gallery',
-          array(
-            'labels' => array(
-              'name' => __( 'Galleries' ),
-              'singular_name' => __( 'Gallery' )
-            ),
-            'public' => true,
-            'has_archive' => false,
-            'rewrite' => array('slug' => 'pictures'),
-          )
-        );
-      }
-      add_action( 'init', 'create_post_type' );
+      register_post_type( 'ps_gallery',
+        array(
+          'labels' => array(
+            'name' => __( 'Galleries' ),
+            'singular_name' => __( 'Gallery' )
+          ),
+          'public' => true,
+          'has_archive' => false,
+          'rewrite' => array('slug' => 'pictures'),
+        )
+      );
+    }
+    add_action( 'init', 'create_post_type' );
