955d05d41914bca631ed50c67981a21d8a9e7206
Sam Thomson <samt@samt.st>
2018-04-29 13:26:09 +0100

    refactor

--- www-workspace/wp-content/themes/picture-site/single-ps_gallery.php
+++ www-workspace/wp-content/themes/picture-site/single-ps_gallery.php
@@ -19,12 +19,16 @@
                 'orderby' => 'menu_order'
             ]);
 
+            $sGalleryHTML = '';
+
             foreach ( $attachments as $id  => $attachment ) {
                 $title = esc_html($attachment->post_title, 1);
                 $img = wp_get_attachment_image_src($id, $THUMB_SIZE);
         
-                echo '<div class="single-image"><img src="' . esc_url( $img[0] ) . '" alt="' . esc_attr( $title ) . '" title="' . esc_attr( $title ) . '" /></div>';
+                $sGalleryHTML .= '<div class="single-image"><img src="' . esc_url( $img[0] ) . '" alt="' . esc_attr( $title ) . '" title="' . esc_attr( $title ) . '" /></div>';
             }
+
+            echo $sGalleryHTML;
   
         ?>
 
