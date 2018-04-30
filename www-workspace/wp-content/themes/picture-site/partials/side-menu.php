abd1440b5bb07faae4aa94414db88a18fe39ae41
Sam Thomson <samt@samt.st>
2018-04-30 13:06:21 +0100

    custom side menu

--- www-workspace/wp-content/themes/picture-site/partials/side-menu.php
+++ www-workspace/wp-content/themes/picture-site/partials/side-menu.php
@@ -2,20 +2,94 @@
     <li><a href="/about">about</a></li>
 </ul>
 
-<ul>
-<?php
+<?php 
 
-    $args = array(
-        'post_type' => 'ps_gallery',
-        'posts_per_page' => -1,
-        'cat' => $iCategoryId
-    );
+    $sqlCategories = "
+    select wp_term_taxonomy.term_id, wp_term_taxonomy.parent, wp_term_taxonomy.count, wp_terms.name, wp_terms.slug from wp_terms
+    right join wp_term_taxonomy  on wp_term_taxonomy.term_id = wp_terms.term_id
+    where wp_term_taxonomy.taxonomy = 'category'
+    ";
 
-    $loop = new WP_Query($args);
+    $sqlPosts = "
+    select wp_posts.ID, wp_posts.post_title, wp_posts.post_name, wp_term_relationships.term_taxonomy_id as category from wp_posts 
+    left join wp_term_relationships on wp_posts.ID = wp_term_relationships.object_id
+    where wp_posts.post_type='ps_gallery' and wp_posts.post_status='publish'
+    ";
 
-    while ($loop->have_posts()) : $loop->the_post();
-        echo '<a href="',the_permalink(),'"><li>', the_title(), '</li></a>';
-    endwhile;
+    $aoCategories = $wpdb->get_results($sqlCategories);
+    $aoPosts = $wpdb->get_results($sqlPosts);
+
+    // iterate through posts
+    $aPostTree = [];
+    foreach($aoPosts as $oPost) {
+        $iCategory = $oPost->category;
+        if (!isset($aPostTree[$iCategory])) {
+            $aPostTree[$iCategory] = [];
+        }
+        $aPost = [
+            'type' => 'gallery',
+            'id' => $oPost->ID,
+            'title' => $oPost->post_title,
+            'name' => $oPost->post_name
+        ];
+        array_push($aPostTree[$iCategory], $aPost);
+    }
+
+    $aTree = [];
+
+    // iterate through categories.
+    $aTree = findChildrenofCategory($aoCategories, 0, $aPostTree);
+
+    function findChildrenofCategory($aCategories, $iFindChildrenOfThisCategoryID, $aPostTree) {
+        // find children of category
+        $aReturn = [];
+        foreach ($aCategories as $oCategory) {
+
+            if ($oCategory->parent == $iFindChildrenOfThisCategoryID) {
+                $oItem = ['type' => 'category', 'name' => $oCategory->name, 'slug' => $oCategory->slug, 'count' => $oCategory->count];
+
+                // // iterate through categories to see if I am parent to any
+                $aChildCategories = findChildrenofCategory($aCategories, $oCategory->term_id, $aPostTree);
+                $aChildrenToAdd = [];
+                // if yes, display it, and repeat
+                if (count($aChildCategories) > 0) {
+                    $aChildrenToAdd = $aChildCategories;
+                }
+                
+                // look for child galleries too
+                if (isset($aPostTree[$oCategory->term_id])) {
+                    $aChildrenToAdd = array_merge($aChildrenToAdd, $aPostTree[$oCategory->term_id]);
+                }
+                    
+                $oItem['children'] = $aChildrenToAdd;
+
+                array_push($aReturn, $oItem);
+            }
+        }
+        return $aReturn;
+    }
+
+    
+    // display tree
+    displayTree($aTree);
+
+    function displayTree($aTree) {
+        echo '<ul>';
+        foreach($aTree as $aBranch) {
+            if ($aBranch['type'] === 'category') {
+                if (isset($aBranch['children']) && count($aBranch['children']) > 0) {
+                    echo '<li>category: ', $aBranch['name'], '</li>';
+                }
+            }
+            if ($aBranch['type'] === 'gallery') {
+                echo '<li>gallery: ', $aBranch['name'], '</li>';
+            }
+
+            if (isset($aBranch['children'])) {
+                displayTree($aBranch['children']);
+            }
+        }
+        echo '</ul>';
+    }
 
 ?>
-</ul>
