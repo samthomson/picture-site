9b33c8dab08f1aacebe79b0a460761f9998ffec1
Sam Thomson <samt@samt.st>
2018-05-03 15:54:04 +0100

    three column layout for galleries

--- www-workspace/wp-content/themes/picture-site/partials/gallery-content.php
+++ www-workspace/wp-content/themes/picture-site/partials/gallery-content.php
@@ -13,11 +13,35 @@
 
     $sGalleryHTML = '';
 
-    foreach ( $attachments as $id  => $attachment ) {
-        $title = esc_html($attachment->post_title, 1);
-        $img = wp_get_attachment_image_src($id, $THUMB_SIZE);
+    $aColumns = [
+        [],
+        [],
+        []
+    ];
 
-        $sGalleryHTML .= '<div class="single-image"><img src="' . esc_url( $img[0] ) . '" alt="' . esc_attr( $title ) . '" title="' . esc_attr( $title ) . '" /></div>';
+    $i = 0;
+    foreach ( $attachments as $id  => $attachment) {
+        $aColumns[$i % 3][$attachment->ID] = $attachment;
+        // array_push($aColumns[$id % 3], $attachment);
+        $i++;
     }
 
-    echo $sGalleryHTML;
+    // echo json_encode($aColumns);
+
+    echo '<div class="column-layout">';
+    foreach ($aColumns as $aColumn) {
+        echo '<div class="single-column"><div class="column-inlet">';
+        foreach ($aColumn as $id  => $attachment) {
+            $title = esc_html($attachment->post_title, 1);
+            $img = wp_get_attachment_image_src($id, $THUMB_SIZE);
+
+            $sGalleryHTML = '';
+
+            $sGalleryHTML .= '<div class="single-image"><img src="' . esc_url( $img[0] ) . '" alt="' . esc_attr( $title ) . '" title="' . esc_attr( $title ) . '" /></div>';
+            echo $sGalleryHTML;
+        }
+        echo '</div></div>';
+    }
+    echo '</div>';
+
+    // echo $sGalleryHTML;
