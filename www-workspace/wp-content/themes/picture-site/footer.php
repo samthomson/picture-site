4fdcf343f43a38315d6688057b3429fa9e6df027
Sam Thomson <samt@samt.st>
2018-11-14 11:02:51 +0630

    suppor optional footer partial for custom scripts

--- www-workspace/wp-content/themes/picture-site/footer.php
+++ www-workspace/wp-content/themes/picture-site/footer.php
@@ -1,5 +1,8 @@
         </div> <!-- /.container -->
 
         <?php wp_footer(); ?> 
+        <b>IS THIS ON EVERY PAGE?</b>
+        <!-- Custom scripts (eg. analytics) -->
+        <?php get_template_part('partials/custom', 'scripts'); ?>
     </body>
 </html>
