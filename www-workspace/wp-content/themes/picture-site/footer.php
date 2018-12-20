a3cc24626a52c885ba9a55a69bffc0337ab15fc8
Sam Thomson <samt@samt.st>
2018-12-20 15:47:26 +0700

    about link

--- www-workspace/wp-content/themes/picture-site/footer.php
+++ www-workspace/wp-content/themes/picture-site/footer.php
@@ -1,5 +1,7 @@
         </div> <!-- /.container -->
 
+        <a href="/about" id="footer-about-link">about this site</a>
+
         <?php wp_footer(); ?> 
         <!-- Custom scripts (eg. analytics) -->
         <?php get_template_part('partials/custom', 'scripts'); ?>
